19fd9a2f9b10c2abac3bc7d899e063c10b3c05ad
CHANDRAPRABHU02 <chandraprabhu1602@gmail.com>
2021-08-24 18:25:16 +0530

    Added input form in crop-recommendation

--- ai-assisted-farming/src/crop_recommendation.jsx
+++ ai-assisted-farming/src/crop_recommendation.jsx
@@ -1,9 +1,113 @@
 import React, { Component } from "react";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import Tooltip from "react-bootstrap/Tooltip";
+import Button from "react-bootstrap/Button";
+import { ReactComponent as InfoCircle } from "bootstrap-icons/icons/info-circle.svg";
+import { ReactComponent as InfoCircleFill } from "bootstrap-icons/icons/info-circle-fill.svg";
 
+const contentInput = [
+  {
+    i: 0,
+    "Short-Description": "Nitrogen Content",
+    Description: "FullDetail-1",
+    Hint: "0 - 140",
+  },
+  {
+    i: 1,
+    "Short-Description": "Phosphorous Content",
+    Description: "FullDetail-2",
+    Hint: "0 - 140",
+  },
+  {
+    i: 2,
+    "Short-Description": "Potassium Content",
+    Description: "FullDetail-1",
+    Hint: "0 - 140",
+  },
+  {
+    i: 3,
+    "Short-Description": "Temperature",
+    Description: "FullDetail-1",
+    Hint: "0 - 140",
+  },
+  {
+    i: 4,
+    "Short-Description": "Humidity",
+    Description: "FullDetail-1",
+    Hint: "0 - 140",
+  },
+  {
+    i: 5,
+    "Short-Description": "PH",
+    Description: "FullDetail-1",
+    Hint: "0 - 140",
+  },
+  {
+    i: 6,
+    "Short-Description": "Rainfall",
+    Description: "FullDetail-1",
+    Hint: "0 - 140",
+  },
+];
 class Crop_recommendation extends Component {
-  state = {};
+  state = {
+    Nitrogen: 1,
+    Phosphorous: null,
+    Potassium: null,
+    Temperature: null,
+    humidity: null,
+    ph: null,
+    rainfall: null,
+  };
+  getInput = () => {
+    return contentInput.map((eachItem) => {
+      return (
+        <div className="row m-3 justify-content-center">
+          <div className="col badge bg-secondary col-12 col-lg-4">
+            <div className="my-info-icon">
+              <OverlayTrigger
+                key="top"
+                placement="top"
+                overlay={
+                  <Tooltip id={"tooltip-top"}>
+                    Tooltip on <strong>Top</strong>.
+                  </Tooltip>
+                }
+              >
+                <InfoCircle />
+              </OverlayTrigger>
+            </div>
+            <div className="h5 my-short-description">
+              {eachItem["Short-Description"]}
+            </div>
+          </div>
+          <div className="col col-lg-4">
+            <div className="form-floating col-12 col-lg-9">
+              <input
+                type="Number"
+                className="form-control"
+                // onChange={(e) => this.handleNameChange(e, i)}
+                placeholder={eachItem.Hint}
+                name={eachItem.Hint}
+                value={this.state.Nitrogen}
+              />
+              <label for="floatingInput">{eachItem.Hint}</label>
+            </div>
+          </div>
+        </div>
+      );
+    });
+  };
+  handleSubmit = () => {
+    console.log(this.state.Nitrogen);
+  };
   render() {
-    return <div>hi</div>;
+    return (
+      <div className="container">
+        {this.getInput()}
+        <button onClick={this.handleSubmit}>Submit</button>
+      </div>
+    );
   }
 }
 
--- ai-assisted-farming/src/navbar.jsx
+++ ai-assisted-farming/src/navbar.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 class Navbar_custom extends Component {
   state = {};
@@ -8,25 +9,17 @@ class Navbar_custom extends Component {
       <div>
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
           <Container>
-            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <Link to="/" className="navbar-brand">
+              AI-Assisted Farming
+            </Link>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
               <Nav className="me-auto">
+                <Link to="/crop_recommendation" className="nav-link">
+                  Crop - recommendation
+                </Link>
                 <Nav.Link href="#features">Features</Nav.Link>
                 <Nav.Link href="#pricing">Pricing</Nav.Link>
-                <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.2">
-                    Another action
-                  </NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.3">
-                    Something
-                  </NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item href="#action/3.4">
-                    Separated link
-                  </NavDropdown.Item>
-                </NavDropdown>
               </Nav>
               <Nav>
                 <Nav.Link href="#deets">More deets</Nav.Link>
